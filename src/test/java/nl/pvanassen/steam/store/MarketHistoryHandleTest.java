82d6a59cfee4a99cfd1b8358ea55120382518b78
Paul van Assen <source@paules.nl>
2014-03-17 19:52:01 +0100

    Test to get the market handle working

---
+++ src/test/java/nl/pvanassen/steam/store/MarketHistoryHandleTest.java
@@ -0,0 +1,14 @@
+package nl.pvanassen.steam.store;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MarketHistoryHandleTest {
+
+    @Test
+    public void testHandle() {
+        MarketHistoryHandle handle = new MarketHistoryHandle(  );
+    }
+
+}
