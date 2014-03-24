1b44b2e10000d07552bb4fa7b8c1009699cf0c66
Paul van Assen <source@paules.nl>
2014-03-24 19:43:01 +0100

    Fixed and enabled history tracking

--- src/main/java/nl/pvanassen/steam/store/MarketHistory.java
+++ src/main/java/nl/pvanassen/steam/store/MarketHistory.java
@@ -6,55 +6,100 @@ import java.util.Date;
  * Items in the market
  * 
  * @author Paul van Assen
- * 
  */
 public class MarketHistory {
-	private final int appId;
-	private final int contextId;
-	private final String urlName;
-	private final Date listed;
-	private final Date acted;
-	private final int price;
-	private final String buyer;
+    private final String steamId;
+    private final int appId;
+    private final int contextId;
+    private final String urlName;
+    private final Date listed;
+    private final Date acted;
+    private final int price;
+    private final String buyer;
 
-	MarketHistory(int appId, int contextId, String urlName, Date listed,
-			Date acted, int price, String buyer) {
-		super();
-		this.appId = appId;
-		this.contextId = contextId;
-		this.urlName = urlName;
-		this.listed = listed;
-		this.acted = acted;
-		this.price = price;
-		this.buyer = buyer;
-	}
+    MarketHistory( String steamId, int appId, int contextId, String urlName, Date listed, Date acted, int price, String buyer ) {
+        super();
+        this.steamId = steamId;
+        this.appId = appId;
+        this.contextId = contextId;
+        this.urlName = urlName;
+        this.listed = listed;
+        this.acted = acted;
+        this.price = price;
+        this.buyer = buyer;
+    }
 
-	public int getAppId() {
-		return appId;
-	}
+    public int getAppId() {
+        return appId;
+    }
 
-	public int getContextId() {
-		return contextId;
-	}
+    public int getContextId() {
+        return contextId;
+    }
 
-	public String getUrlName() {
-		return urlName;
-	}
+    public String getUrlName() {
+        return urlName;
+    }
 
-	public Date getListed() {
-		return listed;
-	}
+    public Date getListed() {
+        return listed;
+    }
 
-	public Date getActed() {
-		return acted;
-	}
+    public Date getActed() {
+        return acted;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public int getPrice() {
+        return price;
+    }
 
-	public String getBuyer() {
-		return buyer;
-	}
+    public String getBuyer() {
+        return buyer;
+    }
+    
+    public String getSteamId() {
+        return steamId;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( steamId == null ) ? 0 : steamId.hashCode() );
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals( Object obj ) {
+        if ( this == obj ) {
+            return true;
+        }
+        if ( obj == null ) {
+            return false;
+        }
+        if ( !( obj instanceof MarketHistory ) ) {
+            return false;
+        }
+        MarketHistory other = ( MarketHistory ) obj;
+        if ( steamId == null ) {
+            if ( other.steamId != null ) {
+                return false;
+            }
+        }
+        else if ( !steamId.equals( other.steamId ) ) {
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/main/java/nl/pvanassen/steam/store/MarketHistoryHandle.java
+++ src/main/java/nl/pvanassen/steam/store/MarketHistoryHandle.java
@@ -1,23 +1,12 @@
 package nl.pvanassen.steam.store;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
+import java.io.*;
 import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.*;
 
 import nl.pvanassen.steam.http.DefaultHandle;
 
@@ -27,17 +16,13 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.cyberneko.html.parsers.DOMFragmentParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.DocumentFragment;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.w3c.dom.html.HTMLDocument;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-
 class MarketHistoryHandle extends DefaultHandle {
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger( getClass() );
     private final List<MarketHistory> marketHistory = new LinkedList<>();
     private static final XPathFactory XPATH_FACTORY = XPathFactory.newInstance();
     private static final XPath XPATH = XPATH_FACTORY.newXPath();
@@ -48,118 +33,119 @@ class MarketHistoryHandle extends DefaultHandle {
     private static final XPathExpression BUYER_XPATH;
 
     static {
-    	XPathExpression historyRowXpath = null;
-    	XPathExpression gainLossXpath = null;
-    	XPathExpression dateXpath = null;
-    	XPathExpression priceXpath = null;
-    	XPathExpression buyerXpath = null;
-    	try {
-    		historyRowXpath = XPATH.compile("//DIV[@class='market_listing_row market_recent_listing_row']");
-    		gainLossXpath = XPATH.compile("//DIV[@class='market_listing_left_cell market_listing_gainorloss']");
-    		dateXpath = XPATH.compile("//DIV[@class='market_listing_right_cell market_listing_listed_date']");
-    		priceXpath = XPATH.compile("//SPAN[@class='market_listing_price']");
-    		buyerXpath = XPATH.compile("//DIV[@class='market_listing_whoactedwith_name_block']");
-		} catch (XPathExpressionException e) {
-			LoggerFactory.getLogger(MarketHistory.class).error("Error instantiating XPATH", e);
-		}
-    	HISTORY_ROW_XPATH = historyRowXpath;
-    	GAIN_LOSS_XPATH = gainLossXpath;
-    	DATE_XPATH = dateXpath;
-    	PRICE_XPATH = priceXpath;
-    	BUYER_XPATH = buyerXpath;
+        XPathExpression historyRowXpath = null;
+        XPathExpression gainLossXpath = null;
+        XPathExpression dateXpath = null;
+        XPathExpression priceXpath = null;
+        XPathExpression buyerXpath = null;
+        try {
+            historyRowXpath = XPATH.compile( "//DIV[@class='market_listing_row market_recent_listing_row']" );
+            gainLossXpath = XPATH.compile( ".//DIV[@class='market_listing_left_cell market_listing_gainorloss']" );
+            dateXpath = XPATH.compile( ".//DIV[@class='market_listing_right_cell market_listing_listed_date']" );
+            priceXpath = XPATH.compile( ".//SPAN[@class='market_listing_price']" );
+            buyerXpath = XPATH.compile( ".//DIV[@class='market_listing_whoactedwith_name_block']" );
+        }
+        catch ( XPathExpressionException e ) {
+            LoggerFactory.getLogger( MarketHistory.class ).error( "Error instantiating XPATH", e );
+        }
+        HISTORY_ROW_XPATH = historyRowXpath;
+        GAIN_LOSS_XPATH = gainLossXpath;
+        DATE_XPATH = dateXpath;
+        PRICE_XPATH = priceXpath;
+        BUYER_XPATH = buyerXpath;
     }
 
     MarketHistoryHandle() {
         super();
     }
-    
+
     private final class HistoryInfo {
-    	private final String listedStr;
-    	private final String actedStr;
-    	private final String price;
-    	private final String buyer;
-		HistoryInfo(String listedStr, String actedStr, String price, String buyer) {
-			super();
-			this.listedStr = listedStr;
-			this.actedStr = actedStr;
-			this.price = price;
-			this.buyer = buyer;
-		}
-    	
+        private final String listedStr;
+        private final String actedStr;
+        private final String price;
+        private final String buyer;
+
+        HistoryInfo( String listedStr, String actedStr, String price, String buyer ) {
+            super();
+            this.listedStr = listedStr;
+            this.actedStr = actedStr;
+            this.price = price;
+            this.buyer = buyer;
+        }
+
     }
-    
+
     @Override
-    public void handle(InputStream stream) throws IOException {
+    public void handle( InputStream stream ) throws IOException {
         ObjectMapper om = new ObjectMapper();
         JsonNode node = om.readTree( stream );
-        String resultHtml = node.get("results_html").asText();
-        Map<String,HistoryInfo> htmlMap = new HashMap<>();
+        String resultHtml = node.get( "results_html" ).asText();
+        Map<String, HistoryInfo> htmlMap = new HashMap<>();
 
         DOMFragmentParser parser = new DOMFragmentParser();
         HTMLDocument document = new HTMLDocumentImpl();
         try {
-        	DocumentFragment fragment = document.createDocumentFragment();
-        	parser.parse(new InputSource( new StringReader( resultHtml ) ), fragment );
-        	
-        	
-        	NodeList nodeSet = (NodeList) HISTORY_ROW_XPATH.evaluate(fragment, XPathConstants.NODESET);
-        	for(int i=0; i<nodeSet.getLength() ; i++) {
-        		Node historyRow = nodeSet.item(i);
-        		String rowName = historyRow.getAttributes().getNamedItem("id").getTextContent();
-        		Node gainLoss = (Node)GAIN_LOSS_XPATH.evaluate(historyRow, XPathConstants.NODE);
-        		if (!"-".equals(gainLoss.getTextContent())) {
-        			// No sale
-        			continue;
-        		}
-        		NodeList dates = (NodeList)DATE_XPATH.evaluate(historyRow, XPathConstants.NODESET);
-        		String listedStr = dates.item(0).getTextContent();
-        		String actedStr = dates.item(0).getTextContent();
-        		String price = ((Node)PRICE_XPATH.evaluate(historyRow, XPathConstants.NODE)).getTextContent();
-        		String buyer = ((Node)BUYER_XPATH.evaluate(historyRow, XPathConstants.NODE)).getTextContent();
-        		htmlMap.put(rowName, new HistoryInfo(listedStr, actedStr, price, buyer));
-        	}
+            DocumentFragment fragment = document.createDocumentFragment();
+            parser.parse( new InputSource( new StringReader( resultHtml ) ), fragment );
+
+            NodeList nodeSet = ( NodeList ) HISTORY_ROW_XPATH.evaluate( fragment, XPathConstants.NODESET );
+            for ( int i = 0; i < nodeSet.getLength(); i++ ) {
+                Node historyRow = nodeSet.item( i );
+                String rowName = historyRow.getAttributes().getNamedItem( "id" ).getTextContent();
+                Node gainLoss = ( Node ) GAIN_LOSS_XPATH.evaluate( historyRow, XPathConstants.NODE );
+                if ( !"-".equals( gainLoss.getTextContent().trim() ) ) {
+                    // No sale
+                    continue;
+                }
+                NodeList dates = ( NodeList ) DATE_XPATH.evaluate( historyRow, XPathConstants.NODESET );
+                String listedStr = dates.item( 0 ).getTextContent().trim();
+                String actedStr = dates.item( 0 ).getTextContent().trim();
+                String price = ( ( Node ) PRICE_XPATH.evaluate( historyRow, XPathConstants.NODE ) ).getTextContent().trim();
+                String buyer = ( ( Node ) BUYER_XPATH.evaluate( historyRow, XPathConstants.NODE ) ).getTextContent().replace( "Buyer:", "" ).trim();
+                htmlMap.put( rowName, new HistoryInfo( listedStr, actedStr, price, buyer ) );
+            }
         }
-        catch (SAXException | XPathExpressionException e) {
-        	logger.error("Error parsing html", e);
-		}
-        Map<String,String> hoverMap = new HashMap<>();
-        String hovers = node.get("hovers").asText();
-        for (String hoverStr : hovers.split(";")) {
-        	String hover = hoverStr.trim();
-        	if (hover.startsWith("CreateItemHoverFromContainer")) {
-        		// CreateItemHoverFromContainer( g_rgAssets, 'history_row_2853334817499652196_2853334817499652205_name', 753, '6', '619156810', 0 );
-        		String[] items = hover.replaceAll("'", "").split(",");
-        		hoverMap.put(items[4], items[1]);
-        	}
+        catch ( SAXException | XPathExpressionException e ) {
+            logger.error( "Error parsing html", e );
         }
-        SimpleDateFormat formatter = new SimpleDateFormat("d M", Locale.US);
-        JsonNode assets = node.get("assets");
-        for (JsonNode appId : assets) {
-            for (JsonNode contextId : appId) {
-                for (JsonNode item : contextId) {
-                	String historyRowId = hoverMap.get(item.get("id"));
-                	HistoryInfo historyInfo = htmlMap.get(historyRowId);
-                	if (historyInfo == null) {
-                		// Not a sale
-                		continue;
-                	}
+        Map<String, String> hoverMap = new HashMap<>();
+        String hovers = node.get( "hovers" ).asText();
+        for ( String hoverStr : hovers.split( ";" ) ) {
+            String hover = hoverStr.trim();
+            if ( hover.startsWith( "CreateItemHoverFromContainer" ) && !hover.contains( "_image" ) ) {
+                // CreateItemHoverFromContainer( g_rgAssets, 'history_row_2853334817499652196_2853334817499652205_name', 753, '6', '619156810', 0 );
+                String[] items = hover.replaceAll( "'", "" ).split( "," );
+                hoverMap.put( items[ 4 ].trim(), items[ 1 ].trim().replace( "_name", "" ) );
+            }
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat( "d MMM", Locale.US );
+        JsonNode assets = node.get( "assets" );
+        for ( JsonNode appId : assets ) {
+            for ( JsonNode contextId : appId ) {
+                for ( JsonNode item : contextId ) {
+                    String historyRowId = hoverMap.get( item.get( "id" ).asText() );
+                    HistoryInfo historyInfo = htmlMap.get( historyRowId );
+                    if ( historyInfo == null ) {
+                        // Not a sale
+                        continue;
+                    }
                     String urlName = URLEncoder.encode( item.get( "market_hash_name" ).asText(), "UTF-8" ).replace( "+", "%20" );
                     // 17 Mar
                     try {
-	                    Date acted = formatter.parse(historyInfo.actedStr);
-	                    Date listed = formatter.parse(historyInfo.listedStr);
-	                    // 0,10&#8364;
-	                    int price = Integer.parseInt(historyInfo.price.replace(",", "").replace("&#8364;", "").trim());
-	                    marketHistory.add(new MarketHistory(appId.asInt(), contextId.asInt(), urlName, listed, acted, price, historyInfo.buyer));
+                        Date acted = formatter.parse( historyInfo.actedStr );
+                        Date listed = formatter.parse( historyInfo.listedStr );
+                        // 0,10&#8364;
+                        int price = Integer.parseInt( historyInfo.price.replace( ",", "" ).replace( "€", "" ).trim() );
+                        marketHistory.add( new MarketHistory( historyRowId, appId.asInt(), contextId.asInt(), urlName, listed, acted, price, historyInfo.buyer ) );
                     }
-                    catch (ParseException e) {
-                    	logger.error("Error parsing date", e);
+                    catch ( ParseException e ) {
+                        logger.error( "Error parsing date", e );
                     }
                 }
             }
         }
     }
-    
+
     List<MarketHistory> getMarketHistory() {
         return marketHistory;
     }
