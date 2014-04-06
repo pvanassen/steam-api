0277d15803e69cde531bfff2bd3a6aa8921494ab
Paul van Assen <source@paules.nl>
2014-04-06 17:43:16 +0200

    Added missing exception

---
+++ src/main/java/nl/pvanassen/steam/store/SteamGuardException.java
@@ -0,0 +1,12 @@
+package nl.pvanassen.steam.store;
+
+/**
+ * Steam guard authentication is needed
+ * 
+ * @author Paul van Assen
+ */
+class SteamGuardException extends Exception {
+
+    SteamGuardException() {
+    }
+}
