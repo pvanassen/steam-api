dcbe56ef20e5e6b191830fc607b6d74a236c6b1c
Paul van Assen <source@paules.nl>
2014-04-07 08:45:43 +0200

    Fixes after new data model

--- src/main/java/nl/pvanassen/steam/http/Http.java
+++ src/main/java/nl/pvanassen/steam/http/Http.java
@@ -3,9 +3,7 @@ package nl.pvanassen.steam.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLEncoder;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
@@ -13,17 +11,13 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.*;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.*;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.AbstractHttpMessage;
 import org.slf4j.Logger;
@@ -52,10 +46,10 @@ public class Http {
     private final HttpClientContext context;
     
     private final String cookies;
-
+    
     private Http(String cookies) {
         this.cookies = cookies;
-        globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.BEST_MATCH).build();
+        globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.BEST_MATCH).setSocketTimeout( 10000 ).build();
         context = HttpClientContext.create();
         init();
     }
@@ -193,7 +187,6 @@ public class Http {
         logger.info("Sending cookie " + cookieStr.toString());
         
         httpPost.setEntity(new StringEntity(sb.toString(), ContentType.create("application/x-www-form-urlencoded", "UTF-8")));
-        
         httpPost.setHeader("Cookie", cookieStr.toString());
         CloseableHttpClient httpclient = HttpClients.custom().build();
         CloseableHttpResponse response = null;
