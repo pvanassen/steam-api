4ce068f57008215b89f9f22c5002860b56c0a05f
Paul van Assen <source@paules.nl>
2014-05-15 16:40:27 +0200

    Setup for url name helper

---
+++ src/main/java/nl/pvanassen/steam/store/helper/UrlNameHelper.java
@@ -0,0 +1,16 @@
+package nl.pvanassen.steam.store.helper;
+
+public final class UrlNameHelper {
+    private UrlNameHelper() {
+        super();
+    }
+    
+    /**
+     * Make sure urlName is uniform
+     * @param urlName
+     * @return
+     */
+    public static String getUrlName(String urlName) {
+        return null;
+    }
+}
