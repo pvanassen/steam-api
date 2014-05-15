6bce12146adc6002f06b9564c6ed78b7989ca5db
Paul van Assen <source@paules.nl>
2014-05-15 20:43:11 +0200

    Fix in urlname

--- src/main/java/nl/pvanassen/steam/store/helper/UrlNameHelper.java
+++ src/main/java/nl/pvanassen/steam/store/helper/UrlNameHelper.java
@@ -1,7 +1,6 @@
 package nl.pvanassen.steam.store.helper;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 import nl.pvanassen.steam.error.SteamException;
@@ -12,23 +11,22 @@ import nl.pvanassen.steam.error.SteamException;
  * @author Paul van Assen
  */
 public final class UrlNameHelper {
-    private UrlNameHelper() {
-        super();
-    }
+	private UrlNameHelper() {
+		super();
+	}
 
-    /**
-     * Make sure urlName is uniform
-     * 
-     * @param urlName Url name, encoded or not. 
-     * @return Uniform url
-     */
-    public static String getUrlName(String urlName) {
-        try {
-            String decoded = URLDecoder.decode(urlName, "UTF-8");
-            return URLEncoder.encode(decoded, "UTF-8").replace("+", "%20");
-        }
-        catch (UnsupportedEncodingException e) {
-            throw new SteamException("Encoding not present", e);
-        }
-    }
+	/**
+	 * Make sure urlName is uniform
+	 * 
+	 * @param urlName
+	 *            Url name, encoded or not.
+	 * @return Uniform url
+	 */
+	public static String getUrlName(String urlName) {
+		try {
+			return URLEncoder.encode(urlName, "UTF-8").replace("+", "%20");
+		} catch (UnsupportedEncodingException e) {
+			throw new SteamException("Encoding not present", e);
+		}
+	}
 }
