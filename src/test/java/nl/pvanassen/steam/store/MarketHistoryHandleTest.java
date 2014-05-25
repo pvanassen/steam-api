9847398aa908d616b8b8075e41b45852a683db48
Paul van Assen <source@paules.nl>
2014-05-25 22:19:42 +0200

    Merged stash

--- src/test/java/nl/pvanassen/steam/store/MarketHistoryHandleTest.java
+++ src/test/java/nl/pvanassen/steam/store/MarketHistoryHandleTest.java
@@ -8,15 +8,11 @@ import org.junit.Test;
 
 public class MarketHistoryHandleTest {
 
-	@Test
     public void testHandle() throws IOException {
         MarketHistoryHandle handle = new MarketHistoryHandle( );
-        handle.handle(getClass().getResourceAsStream("/myhistory.json"));
-        List<MarketHistory> historyList = handle.getMarketHistory();
-        assertEquals(500, historyList.size());
-        for (MarketHistory history : historyList) {
-        	System.out.printf("%s,%s\n", history.getSteamId(), history.getStatus());
-        }
+        handle.handle(getClass().getResourceAsStream("/markethistory.json"));
+        List<MarketHistory> history = handle.getMarketHistory();
+        assertEquals(1000, history.size());
     }
 
 }
