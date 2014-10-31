a919bd85d5f56b9e6517a235e5a45cf1d745bc9c
paul <source@paules.nl>
2014-10-31 08:46:58 +0100

    Removed proxy calls. Only getting services supported

--- src/main/java/nl/pvanassen/steam/store/SteamService.java
+++ src/main/java/nl/pvanassen/steam/store/SteamService.java
@@ -1,35 +1,23 @@
 package nl.pvanassen.steam.store;
 
-import java.util.List;
 import java.util.Set;
 
 import nl.pvanassen.steam.http.Http;
-import nl.pvanassen.steam.store.buy.BuyResult;
 import nl.pvanassen.steam.store.buy.BuyService;
 import nl.pvanassen.steam.store.buy.SteamBuyService;
 import nl.pvanassen.steam.store.buyorder.BuyOrderService;
-import nl.pvanassen.steam.store.buyorder.BuyOrderStatus;
 import nl.pvanassen.steam.store.buyorder.SteamBuyOrderService;
-import nl.pvanassen.steam.store.common.BuyOrder;
-import nl.pvanassen.steam.store.common.Item;
-import nl.pvanassen.steam.store.common.Listing;
-import nl.pvanassen.steam.store.history.History;
 import nl.pvanassen.steam.store.history.HistoryService;
 import nl.pvanassen.steam.store.history.SteamHistoryService;
-import nl.pvanassen.steam.store.inventory.InventoryItem;
 import nl.pvanassen.steam.store.inventory.InventoryService;
 import nl.pvanassen.steam.store.inventory.SteamInventoryService;
 import nl.pvanassen.steam.store.item.ItemService;
-import nl.pvanassen.steam.store.item.OverviewItem;
-import nl.pvanassen.steam.store.item.StatDataPoint;
 import nl.pvanassen.steam.store.item.SteamItemService;
-import nl.pvanassen.steam.store.listing.ListingDeque;
 import nl.pvanassen.steam.store.listing.ListingService;
 import nl.pvanassen.steam.store.listing.SteamListingService;
 import nl.pvanassen.steam.store.login.LoginService;
 import nl.pvanassen.steam.store.login.SteamLoginService;
 import nl.pvanassen.steam.store.marketpage.AppIds;
-import nl.pvanassen.steam.store.marketpage.MarketPage;
 import nl.pvanassen.steam.store.marketpage.MarketPageService;
 import nl.pvanassen.steam.store.marketpage.SteamMarketPageService;
 import nl.pvanassen.steam.store.remove.RemoveService;
@@ -37,10 +25,9 @@ import nl.pvanassen.steam.store.remove.SteamRemoveService;
 import nl.pvanassen.steam.store.sell.SellService;
 import nl.pvanassen.steam.store.sell.SteamSellService;
 import nl.pvanassen.steam.store.tradeoffer.SteamTradeofferService;
-import nl.pvanassen.steam.store.tradeoffer.Tradeoffer;
 import nl.pvanassen.steam.store.tradeoffer.TradeofferService;
 
-import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSet;
 
 /**
  * Interface to the steam store
@@ -48,7 +35,6 @@ import com.google.common.base.Optional;
  * @author Paul van Assen
  */
 class SteamService implements StoreService {
-    // private final Logger logger = LoggerFactory.getLogger(getClass());
     private final Http http;
     private final Set<Integer> appIds;
     private final BuyService buyService;
@@ -68,8 +54,8 @@ class SteamService implements StoreService {
      *            For mocking
      */
     SteamService(Http http, String username) {
+        appIds = ImmutableSet.copyOf(AppIds.getAppids());
         this.http = http;
-        appIds = AppIds.getAppids();
         loginService = new SteamLoginService(http);
         marketPageService = new SteamMarketPageService(http, username);
         buyService = new SteamBuyService(http, username);
@@ -87,131 +73,56 @@ class SteamService implements StoreService {
         this(Http.getInstance(cookies, username), username);
     }
 
-    @Override
-    public void acceptTradeOffer(Tradeoffer tradeoffer) {
-        tradeofferService.acceptTradeOffer(tradeoffer);
-    }
-
-    @Override
-    public BuyResult buy(String listingId, int fee, int subTotal) {
-        return buyService.buy(new BuyOrder(0, "", listingId, subTotal, fee));
-    }
-
-    @Override
-    public void cancelBuyOrder(String id) {
-        buyOrderService.cancelBuyOrder(id);
-    }
-
-    @Override
-    public String createBuyOrder(Item item, int currencyId, int priceTotal, int quantity) {
-        return buyOrderService.createBuyOrder(item, currencyId, priceTotal, quantity);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see nl.pvanassen.steam.store.StoreService#getAllItems(nl.pvanassen.steam.store.GenericHandle)
-     */
-    @Override
-    public void getAllItems(GenericHandle<OverviewItem> genericHandle) {
-        itemService.getAllItems(genericHandle);
-    }
-
-    @Override
     public Set<Integer> getAppIds() {
-        return marketPageService.getAppIds();
+        return appIds;
     }
 
-    @Override
-    public void getAsyncNewlyListed(int currency, String country, ListingDeque queue) {
-        listingService.getAsyncNewlyListed(currency, country, queue);
+    public BuyService getBuyService() {
+        return buyService;
     }
 
-    @Override
-    public BuyOrderStatus getBuyOrderStatus(String buyOrderId) {
-        return buyOrderService.getBuyOrderStatus(buyOrderId);
+    public BuyOrderService getBuyOrderService() {
+        return buyOrderService;
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * @see nl.pvanassen.steam.store.StoreService#getCookies()
-     */
-    @Override
-    public String getCookies() {
-        return http.getCookies();
+    public HistoryService getHistoryService() {
+        return historyService;
     }
 
-    @Override
-    public History getHistory(String lastSteamId) {
-        return historyService.getHistory(lastSteamId);
+    public InventoryService getInventoryService() {
+        return inventoryService;
     }
 
-    @Override
-    public List<InventoryItem> getInventory() {
-        return inventoryService.getInventory();
+    public ListingService getListingService() {
+        return listingService;
     }
 
-    @Override
-    public List<InventoryItem> getInventory(int appId) {
-        return inventoryService.getInventory(appId);
+    public ItemService getItemService() {
+        return itemService;
     }
 
-    @Override
-    public List<InventoryItem> getInventory(String username) {
-        return inventoryService.getInventory(username);
-    }
-
-    @Override
-    public List<InventoryItem> getInventory(String username, int appId) {
-        return inventoryService.getInventory(username, appId);
-    }
-
-    @Override
-    public void getItem(int appId, String urlName, GenericHandle<StatDataPoint> dataPointHandle, GenericHandle<Listing> listingHandle) {
-        itemService.getItem(appId, urlName, dataPointHandle, listingHandle);
-    }
-
-    /**
-     * @see nl.pvanassen.steam.store.StoreService#getLoginService()
-     */
-    @Override
     public LoginService getLoginService() {
         return loginService;
     }
 
-    @Override
-    public List<Listing> getNewlyListed(int currency, String country) {
-        return listingService.getNewlyListed(currency, country);
+    public MarketPageService getMarketPageService() {
+        return marketPageService;
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * @see nl.pvanassen.steam.store.StoreService#getOutstandings()
-     */
-    @Override
-    public MarketPage getOutstandings() {
-        return marketPageService.getOutstandings();
+    public SellService getSellService() {
+        return sellService;
     }
 
-    @Override
-    public List<Tradeoffer> getTradeOffers() {
-        return tradeofferService.getTradeOffers();
+    public TradeofferService getTradeofferService() {
+        return tradeofferService;
     }
 
-    @Override
-    public int makeTradeOffer(long partner, List<InventoryItem> me, List<InventoryItem> them, Optional<String> message) {
-        return tradeofferService.makeTradeOffer(partner, me, them, message);
+    public RemoveService getRemoveService() {
+        return removeService;
     }
-
+    
     @Override
-    public boolean removeListing(String listingId) {
-        return removeService.removeListing(listingId);
-    }
-
-    @Override
-    public void sell(String assetId, int appId, String urlName, int contextId, int price) {
-        sellService.sell(assetId, appId, urlName, contextId, price);
+    public String getCookies() {
+        return http.getCookies();
     }
 }
--- src/main/java/nl/pvanassen/steam/store/StoreService.java
+++ src/main/java/nl/pvanassen/steam/store/StoreService.java
@@ -1,23 +1,18 @@
 package nl.pvanassen.steam.store;
 
-import java.util.List;
 import java.util.Set;
 
-import nl.pvanassen.steam.store.buy.BuyResult;
-import nl.pvanassen.steam.store.buyorder.BuyOrderStatus;
-import nl.pvanassen.steam.store.common.Item;
-import nl.pvanassen.steam.store.common.Listing;
-import nl.pvanassen.steam.store.history.History;
-import nl.pvanassen.steam.store.inventory.InventoryItem;
-import nl.pvanassen.steam.store.item.OverviewItem;
-import nl.pvanassen.steam.store.item.StatDataPoint;
-import nl.pvanassen.steam.store.listing.ListingDeque;
+import nl.pvanassen.steam.store.buy.BuyService;
+import nl.pvanassen.steam.store.buyorder.BuyOrderService;
+import nl.pvanassen.steam.store.history.HistoryService;
+import nl.pvanassen.steam.store.inventory.InventoryService;
+import nl.pvanassen.steam.store.item.ItemService;
+import nl.pvanassen.steam.store.listing.ListingService;
 import nl.pvanassen.steam.store.login.LoginService;
-import nl.pvanassen.steam.store.marketpage.MarketPage;
-import nl.pvanassen.steam.store.sell.SellException;
-import nl.pvanassen.steam.store.tradeoffer.Tradeoffer;
-
-import com.google.common.base.Optional;
+import nl.pvanassen.steam.store.marketpage.MarketPageService;
+import nl.pvanassen.steam.store.remove.RemoveService;
+import nl.pvanassen.steam.store.sell.SellService;
+import nl.pvanassen.steam.store.tradeoffer.TradeofferService;
 
 /**
  * Interface defining a connecion to the steam store
@@ -25,182 +20,34 @@ import com.google.common.base.Optional;
  * @author Paul van Assen
  */
 public interface StoreService {
-    void acceptTradeOffer(Tradeoffer tradeoffer);
-
-    /**
-     * Buy a listed item
-     *
-     * @param listingId
-     *            Listing to buy
-     * @param fee
-     *            Fee to pay
-     * @param subTotal
-     *            Sub total, total excluding fee
-     * @return The results of a purchase
-     */
-    BuyResult buy(String listingId, int fee, int subTotal);
-
-    void cancelBuyOrder(String id);
-
-    String createBuyOrder(Item item, int currencyId, int priceTotal, int quantity);
-
-    /**
-     * @param handle
-     *            Handle overview item
-     */
-    void getAllItems(GenericHandle<OverviewItem> handle);
-
-    /**
-     * Get all known app ids
-     * 
-     * @return Returns a set containing all known tradable app ids
-     */
     Set<Integer> getAppIds();
 
-    /**
-     * Retrieve all newly listed
-     * 
-     * @param currency
-     *            Currency to retrieve
-     * @param country
-     *            Country to get listings for
-     *
-     * @param queue
-     *            Queue to add the items to
-     */
-    void getAsyncNewlyListed(int currency, String country, ListingDeque queue);
+    BuyService getBuyService();
 
-    BuyOrderStatus getBuyOrderStatus(String id);
+    BuyOrderService getBuyOrderService();
 
-    /**
-     * Get the current cookies
-     * 
-     * @return Current set of cookies in a String
-     */
-    String getCookies();
+    HistoryService getHistoryService();
 
-    /**
-     * Get sold items currently offered
-     *
-     * @return a list of market history items
-     */
-    History getHistory(String lastSteamId);
+    InventoryService getInventoryService();
 
-    /**
-     * Retrieve all that is in the inventory of the user
-     *
-     * @return List of items in the inventory
-     */
-    List<InventoryItem> getInventory();
+    ListingService getListingService();
 
-    /**
-     * Retrieve the inventory of a user for only one app id
-     *
-     * @param appId
-     *            Retrieve only one app id
-     * @return List of items in the inventory
-     */
-    List<InventoryItem> getInventory(int appId);
-
-    /**
-     * Retrieve all that is in the inventory of a user
-     *
-     * @param username
-     *            The username to get the inventory from
-     * @return List of items in the inventory
-     */
-    List<InventoryItem> getInventory(String username);
-
-    /**
-     * Retrieve all that is in the inventory of a user and an app id
-     *
-     * @param username
-     *            The username to get the inventory from
-     * @param appId
-     *            Retrieve only one app id
-     * @return List of items in the inventory
-     */
-    List<InventoryItem> getInventory(String username, int appId);
-
-    /**
-     * The datapoints will always be called first. Once they are done the
-     * listings handle is called
-     *
-     * @param appId
-     *            Appid of the item to get
-     * @param urlName
-     *            url name of the item to get
-     * @param dataPointHandle
-     *            If a datapoint is found this handle is called
-     * @param listingHandle
-     *            If all datapoints have been processed, the listings are
-     *            handled through this call
-     */
-    void getItem(int appId, String urlName, GenericHandle<StatDataPoint> dataPointHandle, GenericHandle<Listing> listingHandle);
+    ItemService getItemService();
 
     LoginService getLoginService();
 
-    /**
-     * Retrieve the newly listed
-     *
-     * @param currency
-     *            Currency to retrieve
-     * @param country
-     *            Country to get listings for
-     * @return Listings
-     */
-    List<Listing> getNewlyListed(int currency, String country);
+    MarketPageService getMarketPageService();
+
+    SellService getSellService();
+
+    TradeofferService getTradeofferService();
+
+    RemoveService getRemoveService();
 
     /**
-     * Get outstanding items
-     *
-     * @return Outstanding items
-     */
-    MarketPage getOutstandings();
-
-    /**
-     * @return A list of trade offers
-     */
-    List<Tradeoffer> getTradeOffers();
-
-    /**
-     * Make a trade offer of items to a user
+     * Retrieve the current cookies from the HTTP session
      * 
-     * @param partner
-     *            Trading partner ID
-     * @param me
-     *            What do I offer
-     * @param them
-     *            What do they offer
-     * @param message
-     *            A message for the trade
-     * @return The trade offer id
+     * @return The current cookies from the http session
      */
-    int makeTradeOffer(long partner, List<InventoryItem> me, List<InventoryItem> them, Optional<String> message);
-
-    /**
-     * Remove an active listing
-     *
-     * @param listingId
-     *            Listing to remove
-     * @return True if succesful removed
-     */
-    boolean removeListing(String listingId);
-
-    /**
-     * Sell an item in the inventory
-     *
-     * @param assetId
-     *            Asset id to sell
-     * @param appId
-     *            App id to sell
-     * @param urlName
-     *            Url name to sell
-     * @param contextId
-     *            Context id to sell
-     * @param price
-     *            Price to use
-     * @return True if successful, false if not
-     */
-    void sell(String assetId, int appId, String urlName, int contextId, int price) throws SellException;
+    String getCookies();
 }
