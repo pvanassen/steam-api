504217cdbf812ce6e122fd784146b78ce1cd77bb
Paul van Assen <source@paules.nl>
2015-02-01 21:15:23 +0100

    Fixed NPE

--- src/main/java/nl/pvanassen/steam/store/buyorder/BuyOrderHandle.java
+++ src/main/java/nl/pvanassen/steam/store/buyorder/BuyOrderHandle.java
@@ -38,7 +38,7 @@ class BuyOrderHandle extends DefaultHandle {
     public void handle(InputStream stream) throws IOException {
         JsonNode node = objectMapper.readTree(stream);
         logger.info(node.toString());
-        if (node.get("error").asText() != null && !node.get("error").asText().equals("null")) {
+        if (node.get("error") != null && !node.get("error").asText().equals("null")) {
             error = true;
             message = "Unknown: " + node.toString();
         }
